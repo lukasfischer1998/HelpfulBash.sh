0d8ceaea015a12b6a1e443aba9f9995447f49b3b
Ohboy166 <lukifisch@online.de>
2024-04-10 05:41:09 +0200

    added uninstall

--- src/nvimsetup.sh
+++ src/nvimsetup.sh
@@ -6,38 +6,67 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# Install Neovim
-apt update
-apt install -y neovim
+install_neovim() {
+    # Install Neovim
+    apt update
+    apt install -y neovim
 
-# Check installation
-if command -v nvim &>/dev/null; then
-    echo "Neovim has been installed successfully."
-else
-    echo "Error installing Neovim."
-    exit 1
-fi
+    # Check installation
+    if command -v nvim &>/dev/null; then
+        echo "Neovim has been installed successfully."
+    else
+        echo "Error installing Neovim."
+        exit 1
+    fi
 
-# Change to the user's home directory
-user_home=$(getent passwd "$SUDO_USER" | cut -d: -f6)
-if [ -z "$user_home" ]; then
-    echo "Error: Unable to determine user's home directory."
-    exit 1
-fi
-cd "$user_home" || exit
+    # Change to the user's home directory
+    user_home=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+    if [ -z "$user_home" ]; then
+        echo "Error: Unable to determine user's home directory."
+        exit 1
+    fi
+    cd "$user_home" || exit
 
-# Clone Neovim configuration from https://github.com/jdhao/nvim-config
-sudo -u "$SUDO_USER" git clone https://github.com/jdhao/nvim-config .config/nvim
+    # Clone Neovim configuration from https://github.com/jdhao/nvim-config
+    sudo -u "$SUDO_USER" git clone https://github.com/jdhao/nvim-config .config/nvim
 
-# Check clone
-if [ -d "$user_home/.config/nvim" ]; then
-    echo "Neovim configuration has been installed successfully."
-else
-    echo "Error cloning Neovim configuration."
-    exit 1
-fi
+    # Check clone
+    if [ -d "$user_home/.config/nvim" ]; then
+        echo "Neovim configuration has been installed successfully."
+    else
+        echo "Error cloning Neovim configuration."
+        exit 1
+    fi
 
-# Set permissions
-chown -R "$SUDO_USER":"$SUDO_USER" "$user_home/.config/nvim"
+    # Set permissions
+    chown -R "$SUDO_USER":"$SUDO_USER" "$user_home/.config/nvim"
 
-echo "Finished!"
+    echo "Finished installing Neovim!"
+}
+
+uninstall_neovim() {
+    # Remove Neovim and its configuration
+    apt remove -y neovim
+    rm -rf "$user_home/.config/nvim"
+
+    echo "Neovim and its configuration have been uninstalled successfully."
+}
+
+# Ask user whether to install or uninstall Neovim
+read -p "Do you want to install or uninstall Neovim? (install/uninstall): " choice
+
+case "$choice" in
+    install)
+        install_neovim
+        ;;
+    uninstall)
+        uninstall_neovim
+        ;;
+    *)
+        echo "Invalid choice. Please specify 'install' or 'uninstall'."
+        exit 1
+        ;;
+esac
+
+exit 0
+
